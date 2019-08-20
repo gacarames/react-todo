9b5f8ce5cc4c40a1f7297e16a021dd61f3cd9efa
gcarames <gacarames@gmail.com>
2019-08-20 15:07:08 -0300

    first commit 2

---
+++ build/precache-manifest.dcefa76d540b3c6b5dc041c18c0c1675.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f3407adc52e78f531f22c79ea0db87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62dcedbf8996e0091c30",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "62dcedbf8996e0091c30",
+    "url": "/static/js/main.5cf7c723.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
